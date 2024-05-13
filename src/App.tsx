df26dfce21c8e86f0845f506a4c7143cd8b007ec
novik90 <ivan.novikov.1990@list.ru>
2024-05-13 14:32:22 +0300

    add clock

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
-
-import classes from "./App.module.css"
+import React, { useEffect, useState } from 'react';
+import dayjs from 'dayjs';
 
 function App() {
+  const [date, setDate] = useState(dayjs().format("HH:mm:ss"));
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setDate(dayjs().format("HH:mm:ss"));
+    }, 1000);
+    
+    return () => {
+      clearInterval(timer);
+    }
+  }, [])
+
   return (
     <div>
-      <p className={classes.test}>Hello world!</p>
+      <div>
+        <h1>clock</h1>
+        <div>{date}</div>
+      </div>
     </div>
   );
 }
